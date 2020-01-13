1260b3d19ce1c3c99df4464aa1c15cb5f1ea9dbb
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-01-13 21:57:25 +0530

    changing the location of utils.py

--- eComm/eComm/utils.py
+++ eComm/eComm/utils.py
@@ -7,6 +7,18 @@ from django.utils.text import slugify
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for i in range(size))
 
+def unique_order_id_generator(instance):
+    """
+    This is for a Django project and it assumes your instance 
+    has a model with a order_id field.
+    """
+    order_id = f"{random_string_generator(size=10)}"
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(order_id=order_id).exists()
+    if qs_exists:
+        order_id = f"{random_string_generator(size=10)}"
+    return order_id
+
 
 def unique_slug_generator(instance, new_slug=None):
     """
