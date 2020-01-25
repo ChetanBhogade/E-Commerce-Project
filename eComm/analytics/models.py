b2ed2fa8f7383e9bb6657aec8e75bfa1bd940fbb
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-01-25 11:28:19 +0530

    adding ObjectViewed model in analytics app

--- eComm/analytics/models.py
+++ eComm/analytics/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class ObjectViewed(models.Model):
+    user         = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    ip_address   = models.CharField(max_length=220, null=True, blank=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id    = models.PositiveIntegerField()
+    content_object  = GenericForeignKey('content_type', 'object_id')
+    timestamp       = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.content_object} : - Viewed on {self.timestamp}"
+
--- eComm/eComm/settings.py
+++ eComm/eComm/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'accounts',
+    'analytics',
 ]
 
 MIDDLEWARE = [
