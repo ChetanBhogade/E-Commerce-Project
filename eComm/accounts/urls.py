3f7bae7773131cfe60190240b4560661542f0813
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-01-30 21:17:10 +0530

    Adding order history page

--- eComm/accounts/urls.py
+++ eComm/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import account_home_page, login_page, logout_page, register_page, address_create_view
+from orders.views import order_history_page
 
 app_name = 'account'
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('logout/', logout_page, name='logout'),
     path('register/', register_page, name='register'),
     path('address/create/view/', address_create_view, name='address_create_view'),
+    path('order/history', order_history_page, name='order_history'),
 ]
--- eComm/orders/views.py
+++ eComm/orders/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from .models import Order
+from carts.models import Cart
+from accounts.models import BillingProfile
 # Create your views here.
+
+def order_history_page(request):
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    order_obj = Order.objects.filter(billing_profile=billing_profile)
+    context = {
+        'order_obj': order_obj,
+    }
+    return render(request, "orders/order-history-list.html", context=context)
 
 
